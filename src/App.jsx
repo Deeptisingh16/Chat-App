c8f909f43c8194f48756712408b863a287db1aaa
Deepti Singh <deepti.singh_cs19@gla.ac.in>
2024-01-03 10:31:43 +0530

    created sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
+import { Switch } from 'react-router-dom';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import PublicRoute from './components/PublicRoute';
+import PrivateRoute from './components/PrivateRoute';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import { ProfileProvider } from './context/profile.context';
 
 function App() {
   return (
-    <BrowserRouter>
+    <ProfileProvider>
       <Switch>
-        <Route path="/signin">SignIn</Route>
-        <Route path="/">Home</Route>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
       </Switch>
-    </BrowserRouter>
+    </ProfileProvider>
   );
 }
 
---
+++ src/misc/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+const config = {
+  apiKey: 'AIzaSyDv6b9iMsoip__SvUHaJyaEcPrGbRKhDuU',
+  authDomain: 'chat-app-2049c.firebaseapp.com',
+  databaseURL: 'https://chat-app-2049c-default-rtdb.firebaseio.com',
+  projectId: 'chat-app-2049c',
+  storageBucket: 'chat-app-2049c.appspot.com',
+  messagingSenderId: '510677864771',
+  appId: '1:510677864771:web:e43f0628b174b493971ff6',
+};
+
+const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
